07f6ec82d92125c8c401d4ef72134f6019c07542
Tom Lee <thomas.j.lee@gmail.com>
2013-03-09 15:45:18 -0500

    mostly working -- known bug at high end of calibrated range

--- python/pwmcalibrate.py
+++ python/pwmcalibrate.py
@@ -2,7 +2,7 @@ import time, json, curses, wiringpi
 
 PWM_MAX = 1023
 DEFAULT_PIN = 1
-DEFAULT_CALIBRATION_FILENAME = "pwmcalibrate.json"
+DEFAULT_CALIBRATION_FILENAME = "calibration.json"
 
 class PWMCalibrator(object):
 	"""Helper class for calibrating PWM-driven VU meters"""
@@ -34,7 +34,7 @@ class PWMCalibrator(object):
 			wiringpi.pwmWrite(self.pin, self.calibration[bottom_step][1])
 		else:
 			pct_diff = (value - self.calibration[bottom_step][0]) / (1.0 * (self.calibration[bottom_step + 1][0] - self.calibration[bottom_step][0]))
-			pwm_value = self.calibration[bottom_step][1] + round(pct_diff * (self.calibration[bottom_step + 1][1] - self.calibration[bottom_step][1]))
+			pwm_value = int(self.calibration[bottom_step][1] + round(pct_diff * (self.calibration[bottom_step + 1][1] - self.calibration[bottom_step][1])))
 			wiringpi.pwmWrite(self.pin, pwm_value)
 
 
@@ -42,20 +42,20 @@ class PWMCalibrator(object):
 		# clear the calibration list
 		self.calibration = []
 
-		# init curses, preventing delay on keypress
-		stdscr = curses.initscr()
-		curses.cbreak()
-		stdscr.nodelay(1)
-
 		# max the display, ask for trim pot adjustment
 		wiringpi.pwmWrite(self.pin, PWM_MAX)
 		if steps==None:
-			steps = int(raw_input("Calibrate the trim pot until the meter is at maximum, then enter the desired number of steps and press <enter>"))
+			steps = int(raw_input("Calibrate the trim pot until the meter is at maximum, then enter the desired number of steps and press <enter>: "))
 		else:
 			raw_input("Calibrate the trim pot until the meter is at maximum (%d), then press <enter>" % steps)
 		
 		# set the top step
 		self.calibration.append((steps, PWM_MAX))
+
+		# init curses, preventing delay on keypress
+		stdscr = curses.initscr()
+		curses.cbreak()
+		stdscr.nodelay(1)
 
 		# step down through the PWM range
 		current_step = steps - 1
@@ -67,9 +67,10 @@ class PWMCalibrator(object):
 			stdscr.addstr(2,0,"=== Captured Calibration Values ===")
 			stdscr.addstr(3,0,"      step      |        value     ")
 			stdscr.addstr(4,0,"-----------------------------------")
-			for (i,x) in enumerate(self.calibration):
-				std.addstr(4+i, 7 - len(str(x[0])), str(x[0]))
-				std.addstr(4+i, 30 - len(str(x[1])), str(x[1]))
+			for (j,x) in enumerate(self.calibration):
+				stdscr.addstr(5+j, 7 - len(str(x[0])), str(x[0]))
+				stdscr.addstr(5+j, 16, "|")
+				stdscr.addstr(5+j, 30 - len(str(x[1])), str(x[1]))
 			stdscr.refresh()
 
 			key = stdscr.getch()
@@ -78,7 +79,7 @@ class PWMCalibrator(object):
 				current_step = current_step - 1
 				if current_step<0:
 					break
-			time.sleep(0.05)
+			time.sleep(0.1)
 
 		# append an entry for zero if one wasn't recorded
 		found_zero = False
@@ -92,7 +93,7 @@ class PWMCalibrator(object):
 		curses.endwin()
 
 		# sort the calibration list
-		self.calibration.sort(lambda x: x[0])
+		self.calibration.sort(key=lambda x: x[0])
 
 
 	def save(self, filename=DEFAULT_CALIBRATION_FILENAME):
