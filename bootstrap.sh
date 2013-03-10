8b4103f6a032c9268a332e325eec1b0a97083218
sbma44 <thomas.j.lee@gmail.com>
2013-03-10 15:52:03 -0400

    whoops -- wiringpi should be installed by pip

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,11 +8,11 @@ mkdir ~/.ssh
 curl https://raw.github.com/sbma44/rpi/master/tom_lee_dsa_key.pub >> ~/.ssh/authorized_keys
 
 # python / git
-sudo apt-get -y install python-setuptools git python-dev wiringpi
+sudo apt-get -y install python-setuptools python-dev git 
 sudo easy_install pip
 
 # virtualenv
-sudo pip install virtualenvwrapper
+sudo pip install virtualenvwrapper wiringpi
 mkdir ~/Devel
 mkdir ~/.virtualenvs
 curl https://raw.github.com/sbma44/rpi/master/rpi_virtualenvconfig.txt >> ~/.bashrc
