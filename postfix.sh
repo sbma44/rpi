a7c9954461b6931e10b62b9099db87560c7934ae
sbma44 <thomas.j.lee@gmail.com>
2013-08-27 13:08:18 -0400

    removing sudo's; demanding that script be run as root to make redirects work

--- postfix.sh
+++ postfix.sh
@@ -1,6 +1,12 @@
-sudo aptitude -y install postfix mailutils libsasl2-2 ca-certificates libsasl2-modules
+# test for root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
-sudo echo "relayhost = [smtp.gmail.com]:587
+aptitude -y install postfix mailutils libsasl2-2 ca-certificates libsasl2-modules
+
+echo "relayhost = [smtp.gmail.com]:587
 smtp_sasl_auth_enable = yes
 smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
 smtp_sasl_security_options = noanonymous
@@ -11,13 +17,13 @@ echo "Enter @gmail.com username: "
 read username
 echo "Enter @gmail.com password: "
 read password
-sudo echo "[smtp.gmail.com]:587    $username@gmail.com:$password" > /etc/postfix/sasl_passwd
+echo "[smtp.gmail.com]:587    $username@gmail.com:$password" > /etc/postfix/sasl_passwd
 
-sudo chmod 400 /etc/postfix/sasl_passwd
-sudo postmap /etc/postfix/sasl_passwd
+chmod 400 /etc/postfix/sasl_passwd
+postmap /etc/postfix/sasl_passwd
 
-cat /etc/ssl/certs/Thawte_Premium_Server_CA.pem | sudo tee -a /etc/postfix/cacert.pem
+cat /etc/ssl/certs/Thawte_Premium_Server_CA.pem | tee -a /etc/postfix/cacert.pem
 
-sudo /etc/init.d/postfix reload
+/etc/init.d/postfix reload
 
-echo "Test mail from postfix" | mail -s "Postfix configuration successful!" thomas.j.lee@gmail.com
+echo "Test mail from postfix" | mail -s "Postfix configuration successful" thomas.j.lee@gmail.com
