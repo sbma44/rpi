2b26a3e2a8ca5da23b933507bc19d145f0bc1a18
sbma44 <thomas.j.lee@gmail.com>
2013-09-28 18:12:01 -0400

    phantom js setup script

---
+++ phantomjs.sh
@@ -0,0 +1,19 @@
+cd /tmp
+curl https://github.com/aeberhardo/phantomjs-linux-armv6l/archive/master.zip -O
+unzip master.zip
+cd phantomjs-linux-armv6l-master
+bunzip2 *.bz2 && tar xf *.tar
+mv ./phantomjs-1.9.0-linux-armv6l/bin/phantomjs /usr/bin/
+rm -rf /tmp/phantomjs*
+rm -rf /tmp/master.zip
+
+cd /usr/share
+sudo mv fonts fonts.bak
+sudo mkdir fonts
+
+sudo apt-get install --reinstall ttf-mscorefonts-installer
+
+sudo rm /usr/share/fonts/truetype/msttcorefonts/andalemo.ttf
+sudo rm /usr/share/fonts/truetype/msttcorefonts/Andale_Mono.ttf
+
+sudo fc-cache -rv
