fab7106b5053489bd63505c91b0c1829562c06de
sbma44 <thomas.j.lee@gmail.com>
2013-10-05 12:43:17 -0400

    resolving merge conflict w/ swapfile disabling

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,13 @@
+HOME=/home/pi
+
 # baseline update / tools
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install vim screen libnss-mdns 
 
 # ssh key
-mkdir ~/.ssh
-curl https://raw.github.com/sbma44/rpi/master/tom_lee_dsa_key.pub >> ~/.ssh/authorized_keys
+mkdir $HOME/.ssh
+curl https://raw.github.com/sbma44/rpi/master/tom_lee_dsa_key.pub >> $HOME/.ssh/authorized_keys
 
 # python / git
 sudo apt-get -y install python-setuptools python-dev git 
@@ -13,12 +15,13 @@ sudo easy_install pip
 
 # virtualenv
 sudo pip install virtualenvwrapper wiringpi wiringpi2
-mkdir ~/Devel
-mkdir ~/.virtualenvs
-curl https://raw.github.com/sbma44/rpi/master/rpi_virtualenvconfig.txt >> ~/.bashrc
-source ~/.bashrc
+mkdir $HOME/Devel
+mkdir $HOME/.virtualenvs
+curl https://raw.github.com/sbma44/rpi/master/rpi_virtualenvconfig.txt >> $HOME/.bashrc
+source $HOME/.bashrc
 
 # wifi & preferred networks
+pip install pycrypto
 sudo python setup_wireless.py
 
 # email
@@ -28,6 +31,7 @@ sudo postfix.sh
 sudo swapoff --all
 sudo apt-get -y remove dphys-swapfile
 
+# watchdog
+sudo python watchdog.py
+
 # TODO: more improvements from http://raspberrypi.stackexchange.com/questions/169/how-can-i-extend-the-life-of-my-sd-card
-
-
